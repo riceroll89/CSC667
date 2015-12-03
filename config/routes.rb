fd24c09bd9a9448aea623492748784cf58555bac
ToruNagao <inmylife777@gmail.com>
2015-12-02 18:10:44 -0800

    added clickable links that take you from index.html to game.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  get 'game/game', to: 'game#game'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -10,7 +12,6 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
